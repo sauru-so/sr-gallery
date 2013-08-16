eed56a87a145f6019757d2b58677ff2af34c3a9d
Yonghwan SO <sio4@users.sf.net>
2013-08-16 18:05:22 +0900

    structure for module gallorg.

--- src/so/sauru/sr_gallery/OrganizeActivity.java
+++ src/so/sauru/sr_gallery/OrganizeActivity.java
@@ -1,12 +1,23 @@
 package so.sauru.sr_gallery;
 
+import java.io.File;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Date;
+import java.util.Iterator;
 import java.util.Locale;
+
+import so.sauru.UriUtils;
 
 import android.app.ActionBar;
 import android.app.FragmentTransaction;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.net.Uri;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentManager;
@@ -20,6 +31,10 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView.OnItemSelectedListener;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -40,6 +55,11 @@ public class OrganizeActivity extends FragmentActivity implements
 	 * The {@link ViewPager} that will host the section contents.
 	 */
 	ViewPager mViewPager;
+
+	/* my variables */
+	static ArrayList<Uri> uriList = new ArrayList<Uri> ();
+	ArrayList<File> fileList = new ArrayList<File> ();
+	static String GALLORG = "GallOrg";
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -84,11 +104,13 @@ public class OrganizeActivity extends FragmentActivity implements
 		/* real works */
 		Intent intent = getIntent();
 		Bundle extras = intent.getExtras();
+		uriList.clear();
 		if (Intent.ACTION_SEND.equals(intent.getAction())) {
 			if (extras != null) {
 				Uri fileUri = (Uri) extras.getParcelable(Intent.EXTRA_STREAM);
-				Toast.makeText(this, fileUri.toString(), Toast.LENGTH_LONG);
-				Log.d("GALLORG", "SEND URI:" + fileUri.toString());
+				Log.d(GALLORG, "SEND URI:" + fileUri.toString());
+				uriList.add(fileUri);
+				Log.d(GALLORG, "SEND URI.Path:" + fileUri.getPath());
 			}
 		}
 	}
@@ -145,7 +167,12 @@ public class OrganizeActivity extends FragmentActivity implements
 			// getItem is called to instantiate the fragment for the given page.
 			// Return a DummySectionFragment (defined as a static inner class
 			// below) with the page number as its lone argument.
-			Fragment fragment = new DummySectionFragment();
+			Fragment fragment;
+			if (position == 0) {
+				fragment = new FileSectionFragment();
+			} else {
+				fragment = new DummySectionFragment();
+			}
 			Bundle args = new Bundle();
 			args.putInt(DummySectionFragment.ARG_SECTION_NUMBER, position + 1);
 			fragment.setArguments(args);
@@ -200,4 +227,73 @@ public class OrganizeActivity extends FragmentActivity implements
 		}
 	}
 
+	public static class FileSectionFragment extends Fragment {
+		public FileSectionFragment() {
+		}
+
+		@Override
+		public View onCreateView(LayoutInflater inflater, ViewGroup container,
+				Bundle savedInstanceState) {
+			View rV = inflater.inflate(R.layout.fragment_gallorg_file,
+					container, false);
+			EditText etUri = (EditText) rV.findViewById(R.id.go_file_uri_value);
+			EditText etPath = (EditText) rV.findViewById(R.id.go_file_path_value);
+			EditText etName = (EditText) rV.findViewById(R.id.go_file_name_value);
+			EditText etSize = (EditText) rV.findViewById(R.id.go_file_size_value);
+			EditText etDate = (EditText) rV.findViewById(R.id.go_file_date_value);
+			EditText etHash = (EditText) rV.findViewById(R.id.go_file_hash_value);
+
+			/** get file information and show it **/
+			Log.d(GALLORG, uriList.toString());
+			if (uriList.size() == 1) {
+				Uri tU = uriList.get(0);
+				File tF = UriUtils.getFileFromUri(tU, this.getActivity());
+				SimpleDateFormat dF = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+				etUri.setText(tU.toString());
+				etPath.setText(tF.getParent());
+				etName.setText(tF.getName());
+				etSize.setText(tF.length()/1024 + "KB");
+				etDate.setText(dF.format(new Date(tF.lastModified())));
+				etHash.setText("Not implemented yet");
+			} else if (uriList.size() > 0) {
+				etUri.setText("Bulk");
+				etPath.setText("Bulk");
+				etName.setText("Bulk");
+			} else {
+				etUri.setText("Empty");
+				etPath.setText("Empty");
+				etName.setText("Empty");
+				Log.e(GALLORG, "Error! empty URI list: " + uriList.toString());
+			}
+
+			/** get directory from... **/
+			SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this.getActivity());
+			File gRoot = new File(prefs.getString("gallorg_root", "/"));
+			Log.d(GALLORG, gRoot.getPath());
+
+			/* generate dir list */
+			ArrayList<String> dirStrList = new ArrayList<String> ();
+			dirStrList.add("<Camera>");
+			if (gRoot.exists() && gRoot.isDirectory()) {
+				ArrayList<File> dirList = new ArrayList<File> (Arrays.
+						asList(gRoot.listFiles()));
+				Collections.sort(dirList);
+				Iterator<File> e = dirList.iterator();
+				while (e.hasNext()) {
+					dirStrList.add(((File) e.next()).getName());
+				}
+				Log.d(GALLORG, dirStrList.toString());
+			}
+			dirStrList.add("New");
+
+			Spinner spAlbums = (Spinner) rV.findViewById(R.id.og_albums_spinner);
+			ArrayAdapter<String> aa = new ArrayAdapter <String> (this.getActivity(),
+					android.R.layout.simple_spinner_dropdown_item, dirStrList);
+			spAlbums.setAdapter(aa);
+			//spAlbums.setOnItemSelectedListener(this);
+
+			//Toast.makeText(this, uriList.get(0).toString(), Toast.LENGTH_LONG).show();
+			return rV;
+		}
+	}
 }
