7ea222e7891692628637872bd6131f15de4a0ee1
Yonghwan SO <sio4@users.sf.net>
2013-08-19 19:23:19 +0900

    rename menu resource for gallorg.

--- src/so/sauru/sr_gallery/OrganizeActivity.java
+++ src/so/sauru/sr_gallery/OrganizeActivity.java
@@ -132,7 +132,7 @@ public class OrganizeActivity extends FragmentActivity implements
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.organize, menu);
+		getMenuInflater().inflate(R.menu.gallorg, menu);
 		return true;
 	}
 
